3c28f990cb568e9d7bee230375877e0d9ea147dd
Chrain <chrain.master@gmail.com>
2015-10-09 13:37:17 +0800

    fix bugs

--- Classes/BMGlobalMacro.h
+++ Classes/BMGlobalMacro.h
@@ -51,7 +51,7 @@ typedef id   (^IDBlock_id)  (id);
 #define RGBAlpha(r, g, b, a) [UIColor colorWithRed:((r) / 255.0) green:((g) / 255.0) blue:((b) / 255.0) alpha:(a)]
 #define HexRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define HexRGBAlpha(rgbValue, a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:(a)]
-#define RandomColor      BMColor(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256), 1.f)
+#define RandomColor      RGB(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
 
 #define SCREEN_WIDTH  ([UIScreen mainScreen].bounds.size.width)
 #define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
